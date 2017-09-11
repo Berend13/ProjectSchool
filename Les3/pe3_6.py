4e4ae1514d3136c151c6c47f2287ae19b0ac30b3
Berend <berend.sliedrecht@student.hu.nl>
2017-09-11 16:37:49 +0200

    Alles tot en met 4_1

--- Les3/pe3_6.py
+++ Les3/pe3_6.py
@@ -1,13 +1,7 @@
 s = "Guido van Rossum heeft programmeertaal Python bedacht."
 
-for a in s:
-    print('a')
-for o in s:
-    print('o')
-for e in s:
-    print('e')
-for i in s:
-    print('i')
-for u in s:
-    print('u')
-print('Ik snapte deze niet, of forloops in het algemeen.')
+for letter in s:
+    if letter in 'aeiou':
+        print(letter)
+
+
